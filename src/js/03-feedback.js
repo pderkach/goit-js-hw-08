f82890693d46fb66f1490fc895a0e961adc32be2
Paul Derkach <mr.derkach.55@gmail.com>
2023-08-27 12:54:46 +0300

    finished all tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageTextarea = form.querySelector('[name="message"]');
+
+const LS_KEY = 'feedback-form-state';
+
+const savedFormState = JSON.parse(localStorage.getItem(LS_KEY));
+if (savedFormState) {
+  emailInput.value = savedFormState.email;
+  messageTextarea.value = savedFormState.message;
+}
+
+const updateLocalStorage = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(LS_KEY, JSON.stringify(formState));
+}, 500);
+
+form.addEventListener('input', updateLocalStorage);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  localStorage.removeItem(LS_KEY);
+  emailInput.value = '';
+  messageTextarea.value = '';
+  console.log('Форму відправлено з такими даними:', {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  });
+});
