723ca0b669f3927e97cd441e5c20a0b728f5a709
Paul Derkach <mr.derkach.55@gmail.com>
2023-08-27 12:42:36 +0300

    completed second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerElement = document.getElementById('vimeo-player');
+
+const player = new Player(playerElement);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const savedTime = localStorage.getItem(STORAGE_KEY);
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+const updateAndStoreTime = throttle(async () => {
+  const currentTime = await player.getCurrentTime();
+  localStorage.setItem(STORAGE_KEY, currentTime);
+}, 1000); 
+
+player.on('timeupdate', updateAndStoreTime);
